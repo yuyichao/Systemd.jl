1ec2b9551fbd3723236629fc6789907f3eb9a1ec
Yichao Yu <yyc1992@gmail.com>
2017-03-09 21:40:47 -0500

    -

--- travis/runtest.sh
+++ travis/runtest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-envs=$(declare -xp | grep 'export TRAVIS_')
-
-docker exec $(cat /docker-name) bash -c "$envs; cd $PWD; exec bash $PWD/travis/docker_test.sh"
+envs=$(declare -xp | grep 'TRAVIS_')
+sudo docker exec $(cat /docker-name) bash -c "$envs; cd $PWD; exec bash $PWD/travis/docker_test.sh"
 ret=$?
-docker kill $(cat /docker-name)
+sudo docker kill $(cat /docker-name)
 exit $ret
